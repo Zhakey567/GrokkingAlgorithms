b65adbb0991f3423f18ce644a03498b1b16c45e5
Timur.Zhakeev <timur7zhakeev@gmail.com>
2023-04-03 17:56:59 +0500

    '1.0'

--- Program.cs
+++ Program.cs
@@ -1,11 +1,37 @@
-﻿namespace GrokkingAlgorithms
+﻿using System.Runtime.CompilerServices;
+
+namespace GrokkingAlgorithms
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            var a = "asd";
+            var arr = new int[] { 1, 1, 0, 1, 1, 1 };
+            var a = FindMaxConsecutiveOnes(arr);
+            Console.WriteLine(a);
         }
+
+        public static int FindMaxConsecutiveOnes(int[] nums)
+        {
+            var count = 0;
+            var maxCount = 0;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 1)
+                    count++;
+                else if (count > maxCount)
+                {
+                    maxCount = count;
+                    count = 0;
+                }
+                else
+                    count = 0;
+                if (i == nums.Length - 1 && count > maxCount)
+                {
+                    maxCount = count;
+                }
+            }
+            return maxCount;
+        }//1 1 0 1 1 1
     }
 }
